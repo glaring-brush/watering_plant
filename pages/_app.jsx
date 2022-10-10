c52e1e4074137d6ee23a5b1c7b6be88cadf234cd
glaring-brush <yuriy.tupychak@startupsoft.com>
2022-10-10 10:29:36 +0300

    Fix(?) page transitions

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,32 +1,36 @@
-import { store, persistor } from '../store';
+import { persistor, store } from '../store';
 import '../styles/globals.css';
 import { useRouter } from 'next/router';
 import 'normalize.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-function setBodyOpacity(opacity = 1) {
-  document.body.style.opacity = opacity;
+function setPageTransition(isTransitioning) {
+  if (isTransitioning) {
+    document.body.classList.add('changingPage');
+  } else {
+    document.body.classList.remove('changingPage');
+  }
 }
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  // useEffect(() => {
-  //   const handleStart = (url) => url !== router.asPath && setBodyOpacity(0.1);
-  //   const handleComplete = (url) => url === router.asPath && setBodyOpacity();
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setPageTransition(true);
+    const handleComplete = () => setPageTransition(false);
 
-  //   router.events.on('routeChangeStart', handleStart);
-  //   router.events.on('routeChangeComplete', handleComplete);
-  //   router.events.on('routeChangeError', handleComplete);
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
 
-  //   return () => {
-  //     router.events.off('routeChangeStart', handleStart);
-  //     router.events.off('routeChangeComplete', handleComplete);
-  //     router.events.off('routeChangeError', handleComplete);
-  //   };
-  // });
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  });
 
   return (
     <Provider store={store}>
